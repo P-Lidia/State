33d081af3b0c42f9e9be832825153b05ffb46d53
P-Lidia <Lydiasmolyakova@gmail.com>
2024-11-22 12:27:04 +0300

    Created method printRegionNumber()

--- src/main/java/ru/plidia/state/_main/_Main.java
+++ src/main/java/ru/plidia/state/_main/_Main.java
@@ -11,33 +11,29 @@ public class _Main {
         Citizen citizen = new Citizen();
         List<Citizen> citizens = new ArrayList<>();
         int count = (int) (Math.random() * 100);
-        for(int i = 0; i <= count; i++) {
+        for (int i = 0; i <= count; i++) {
             citizens.add(citizen);
         }
         City city = new City("Asdsds", citizens.size(), citizens);
         List<City> cities = new ArrayList<>();
         count = (int) (Math.random() * 10);
-        for(int i = 0; i <= count; i++) {
+        for (int i = 0; i <= count; i++) {
             cities.add(city);
         }
-        District district = new District("Sfdfdf",cities.size(), cities);
+        District district = new District("Sfdfdf", cities.size(), cities);
         List<District> districts = new ArrayList<>();
         count = (int) (Math.random() * 4);
-        for(int i = 0; i <= count; i++) {
+        for (int i = 0; i <= count; i++) {
             districts.add(district);
         }
         Region region = new Region("Ysdfc", districts.size(), districts);
         List<Region> regions = new ArrayList<>();
         count = (int) (Math.random() * 2);
-        for(int i = 0; i <= count; i++) {
+        for (int i = 0; i <= count; i++) {
             regions.add(region);
         }
-        Capital capital = new Capital("Capital",citizens.size(), citizens);
+        Capital capital = new Capital("Capital", citizens.size(), citizens);
         State.getInstance(regions, capital);
-
-
-
-
 
 
     }
--- src/main/java/ru/plidia/state/util/PrintRequest.java
+++ src/main/java/ru/plidia/state/util/PrintRequest.java
@@ -1,17 +1,19 @@
 package ru.plidia.state.util;
 
 import ru.plidia.state.entity.Capital;
+import ru.plidia.state.entity.Region;
+
+import java.util.List;
+
 
 public class PrintRequest {
     public void printCapital(Capital capital) {
-        System.out.println(capital);
+        System.out.println("Столица государства: " + capital);
     }
 
-
-
-
-
-
+    public void printRegionNumber(List<Region> regionList) {
+        System.out.println("Количество областей :" + regionList.size());
+    }
 
 
 }

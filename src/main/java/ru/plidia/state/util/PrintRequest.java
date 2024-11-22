58a47cfcabd850a840861cfdef4a3b05b76e0ecc
P-Lidia <Lydiasmolyakova@gmail.com>
2024-11-22 12:15:56 +0300

    Created class PrintRequest and method printCapital()

---
+++ src/main/java/ru/plidia/state/util/PrintRequest.java
@@ -0,0 +1,17 @@
+package ru.plidia.state.util;
+
+import ru.plidia.state.entity.Capital;
+
+public class PrintRequest {
+    public void printCapital(Capital capital) {
+        System.out.println(capital);
+    }
+
+
+
+
+
+
+
+
+}

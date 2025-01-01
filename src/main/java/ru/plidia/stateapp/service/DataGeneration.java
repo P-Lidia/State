73a0b3bd65d0dd26ced87b72e58883c80adc7959
P-Lidia <Lydiasmolyakova@gmail.com>
2025-01-01 21:22:33 +0300

    fix DataGeneration class

--- src/main/java/ru/plidia/stateapp/service/DataGeneration.java
+++ src/main/java/ru/plidia/stateapp/service/DataGeneration.java
@@ -9,6 +9,32 @@ import java.util.List;
 import java.util.Random;
 
 public class DataGeneration {
+    public List<District> generateDistrictList(Object o) {
+        List<District> districtList = new ArrayList<>();
+        for (int i = 0; i < (int) (5 + Math.random() * 3); i++) {
+            districtList.add(new District("",
+                    generateCityList(new City())));
+        }
+        return districtList;
+    }
+
+    public List<City> generateCityList(Object o) {
+        List<City> cityList = new ArrayList<>();
+        for (int i = 0; i < (int) (5 + Math.random() * 5); i++) {
+            cityList.add(new City("",
+                    generateCitizenList(new Citizen())));
+        }
+        return cityList;
+    }
+
+    public List<Citizen> generateCitizenList(Object o) {
+        List<Citizen> citizenList = new ArrayList<>();
+        for (int i = 0; i < (int) (1000 + Math.random() * 500); i++) {
+            citizenList.add(new Citizen("", "", 34));
+        }
+        return citizenList;
+    }
+
     public static String generateRandomName(String name) {
         Random random = new Random();
         int capacity = 5 + (int) (Math.random() * 6);
@@ -24,31 +50,5 @@ public class DataGeneration {
 
     public static int generateRandomAge(int age) {
         return (int) (Math.random() * 100);
-    }
-
-    public List<Citizen> generateCitizenList(Object o) {
-        List<Citizen> citizenList = new ArrayList<>();
-        for (int i = 0; i < (int) (1000 + Math.random() * 500); i++) {
-            citizenList.add(new Citizen("", "", 34));
-        }
-        return citizenList;
-    }
-
-    public List<City> generateCityList(Object o) {
-        List<City> cityList = new ArrayList<>();
-        for (int i = 0; i < (int) (5 + Math.random() * 5); i++) {
-            cityList.add(new City("",
-                    generateCitizenList(new Citizen())));
-        }
-        return cityList;
-    }
-
-    public List<District> generateDistrictList(Object o) {
-        List<District> districtList = new ArrayList<>();
-        for (int i = 0; i < (int) (5 + Math.random() * 3); i++) {
-            districtList.add(new District("",
-                    generateCityList(new City())));
-        }
-        return districtList;
     }
 }

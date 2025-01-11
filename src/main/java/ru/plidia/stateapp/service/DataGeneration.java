ca5454cf018f4c49268814fc57ed6c7c0675df41
P-Lidia <Lydiasmolyakova@gmail.com>
2025-01-11 21:55:00 +0300

    fix generateRandomName() method

--- src/main/java/ru/plidia/stateapp/service/DataGeneration.java
+++ src/main/java/ru/plidia/stateapp/service/DataGeneration.java
@@ -39,10 +39,8 @@ public class DataGeneration {
         Random random = new Random();
         int capacity = 5 + (int) (Math.random() * 6);
         StringBuilder sb = new StringBuilder(capacity);
-        for (int i = 0; i < 1; i++) {
-            sb.append((char) ('A' + random.nextInt(25)));
-        }
-        for (int i = 1; i < sb.capacity(); i++) {
+        sb.append((char) ('A' + random.nextInt(25)));
+        for (int i = 0; i < sb.capacity(); i++) {
             sb.append((char) ('a' + random.nextInt(25)));
         }
         return sb.toString();
